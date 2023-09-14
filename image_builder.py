070860ea0952f6541ad67f6fd0210f616da0bdcd
lucas <lucasar.com8@gmail.com>
2023-09-14 10:55:52 +0200

    finish code

--- image_builder.py
+++ image_builder.py
@@ -1,7 +1,9 @@
 import cv2
-import svg_to_png
+#import svg_to_png
 import util
 import numpy as np
+import math
+import random
 
 # name of the piece
 NAME = "8x8"
@@ -11,13 +13,11 @@ CROP_MARGIN = 500
 
 
 def main():
-
-
-
     # convert to png
     svg_path = f".\\SVGs\\{NAME}.svg"
     png_path = f".\\PNGs\\{NAME}.png"
-    svg_to_png.svg_to_png(svg_path,png_path)
+    output_path = f".\\outputs\\{NAME}.png"
+    #svg_to_png.svg_to_png(svg_path,png_path)
 
     # open the image
     img = cv2.imread(png_path)
@@ -40,7 +40,8 @@ def main():
 
     black = np.zeros_like(img)
 
-
+    max_dim = 0
+    all_pieces = []
 
     for i in range(1,retval):
 
@@ -55,12 +56,44 @@ def main():
         y1 = stat[cv2.CC_STAT_TOP] + stat[cv2.CC_STAT_HEIGHT] + CROP_MARGIN
         x1 = stat[cv2.CC_STAT_LEFT] + stat[cv2.CC_STAT_WIDTH] + CROP_MARGIN
 
+        max_dim = max(max_dim, y1-y0)
+        max_dim = max(max_dim, x1-x0)
+
         # crop the piece
         piece = piece[y0:y1,x0:x1]
 
-        cv2.imshow("",piece)
-        cv2.waitKey(0)
+        #rotate the piece
+        piece = util.rotate_image(piece)
 
+        all_pieces.append(piece)
+
+        #cv2.imshow("",piece)
+        #cv2.waitKey(0)
+
+    random.shuffle(all_pieces)
+
+    num_of_pieces = len(all_pieces)
+
+    pieces_per_side = math.ceil(np.sqrt(num_of_pieces))
+    side_len =  (pieces_per_side+1)*max_dim
+
+    result_image = np.zeros(shape=(side_len,side_len),dtype=np.uint8)
+
+    i = 0
+
+    for x in range(pieces_per_side):
+        for y in range(pieces_per_side):
+
+            try:
+                paste_from = all_pieces[i]
+            except: 
+                break
+
+            result_image = util.paste_on_top(result_image,paste_from,x*max_dim,y*max_dim)
+
+            i+=1
+
+    cv2.imwrite(output_path,result_image)
 
 if __name__ == "__main__":
     main()
--- util.py
+++ util.py
@@ -1,4 +1,32 @@
+import cv2
+import random
+import numpy as np
 
 # rotate the image by a random angle
 def rotate_image(image):
-    pass
+    
+    # Get image dimensions
+    height, width = image.shape[:2]
+    
+    # Generate a random angle between -30 and 30 degrees
+    random_angle = random.uniform(-180, 180)
+    
+    # Calculate the rotation matrix
+    rotation_matrix = cv2.getRotationMatrix2D((width/2, height/2), random_angle, 1)
+    
+    # Perform the actual rotation
+    rotated_image = cv2.warpAffine(image, rotation_matrix, (width, height))
+    
+    return rotated_image
+
+def paste_on_top(paste_to: np.ndarray, paste_from: np.ndarray, x: int ,y: int ):
+
+    w = paste_from.shape[1]
+    h = paste_from.shape[0]
+
+    y2 = y+h
+    x2 = x+w
+
+    paste_to[y:y2,x:x2] = paste_from
+
+    return paste_to
